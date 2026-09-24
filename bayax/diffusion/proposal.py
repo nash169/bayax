53b6afd4a21438bc28570f3975075419333164a3
Bernardo <bernardo.fichera@gmail.com>
2026-09-24 16:04:49 +0200

    removed; now in gaussian_kernel

--- bayax/diffusion/proposal.py
+++
@@ -1,7 +0,0 @@
-from bayax.densities import MultivariateNormal
-from bayax.operators import ScaledOperator
-
-
-def proposal(f, t, x, u, dt):
-    drift, cov = f(t=t, x=x, u=u)
-    return MultivariateNormal(cov=ScaledOperator(dt, cov), mean=x + dt * drift)
