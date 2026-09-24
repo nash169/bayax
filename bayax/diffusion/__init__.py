11b877a5efd03024a61284bef0c211421af5b812
Bernardo <bernardo.fichera@gmail.com>
2026-09-24 16:14:54 +0200

    adjusted imports

--- bayax/diffusion/__init__.py
+++ bayax/diffusion/__init__.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from bayax.diffusion.diffusion import diffusion
-from bayax.diffusion.brownian import brownian, brownian_geometric
-from bayax.diffusion.langevin import langevin, langevin_geometric
-from bayax.diffusion.mcmc import mcmc
-from bayax.diffusion.proposal import proposal
+from bayax.diffusion.brownian import brownian, brownian_geom
+from bayax.diffusion.langevin import langevin, langevin_geom
+from bayax.diffusion.kernels import gaussian_kernel
 
 __all__ = [
-    "diffusion",
     "brownian",
-    "brownian_geometric",
+    "brownian_geom",
     "langevin",
-    "langevin_geometric",
-    "mcmc",
-    "proposal",
+    "langevin_geom",
+    "gaussian_kernel",
 ]
