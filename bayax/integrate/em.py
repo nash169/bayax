a91556818d7fa4752cd606b261a46e31cf640447
Bernardo <bernardo.fichera@gmail.com>
2026-09-24 16:19:57 +0200

    now leverages gaussian_kernel in order to compute the next position

--- bayax/integrate/em.py
+++ bayax/integrate/em.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from bayax.diffusion.kernels import gaussian_kernel
 
-def em(f, t, x, u, dt):
-    drift, diffusion = f(t, x, u)
-    return x + dt * drift + diffusion if drift is not None else x + diffusion
+
+def em(f, t, x, u, dt, key, **kwargs):
+    dist = gaussian_kernel(f, t, x, u, dt)
+    return dist.sample(key=key, **kwargs)
+
+
+# def em(f, t, x, u, dt, key, **kwargs):
+#     drift, diffusion = f(t, x, u)
+#     return x + dt * drift + diffusion if drift is not None else x + diffusion
